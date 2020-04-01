298e3bf90a6d860b7ce009ce5ff58fa91f23f7b9
Savio Castro <scastroarmis@intercement.com>
2020-04-01 11:46:06 +0100

    head

---
+++ app.js
@@ -0,0 +1,10 @@
+function mostrarHelp() {
+
+  //console.log(elmnt);
+  if (document.getElementById("help").style.display == 'none')
+    {
+		document.getElementById("help").style.display = '';	
+	} else {
+		document.getElementById("help").style.display = 'none';	
+	}
+}
